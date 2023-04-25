505781b775bd6557e8a5fc0afb510eb0adc69bba
HoangDSR <nvh.02021995@gmail.com>
2023-04-25 17:30:49 +0700

    Fix css, add props

--- src/_codux/boards/product-component/product-component.board.tsx
+++ src/_codux/boards/product-component/product-component.board.tsx
@@ -3,5 +3,15 @@ import { ProductComponent } from '../../../components/product-component/product-
 
 export default createBoard({
     name: 'ProductComponent',
-    Board: () => <ProductComponent nameProduct="The Time Has Come" />,
+    Board: () => (
+        <ProductComponent
+            nameProduct="The Time Has Come"
+            descProduct={
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+            }
+            img={'https://picsum.photos/200/300'}
+            price={'29.53'}
+            priceBefore={'30.25'}
+        />
+    ),
 });
--- src/components/product-component/product-component.tsx
+++ src/components/product-component/product-component.tsx
@@ -4,28 +4,39 @@ import { ButtonComponent } from '../button-component/button-component';
 
 export interface ProductComponentProps {
     className?: string;
+    img: string;
     nameProduct: string;
+    descProduct: string;
+    price: string;
+    priceBefore: string;
 }
 
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-product-components-and-templates
  */
-export const ProductComponent = ({ className, nameProduct }: ProductComponentProps) => {
+export const ProductComponent = ({
+    className,
+    img,
+    nameProduct,
+    descProduct,
+    price,
+    priceBefore,
+}: ProductComponentProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <div className={styles['img-product']}>
-                <img src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg" />
+                <img src={img} />
             </div>
             <div className={styles['detail-product']}>
                 <div className={styles['about-product']}>
                     <h3 className={styles['name-product']}>{nameProduct}</h3>
                     <p className={styles['desc-product']}>
-                        Lindbergh&apos;s Pharmacy is an Athens, Georgia, institution...
+                        {descProduct}
                     </p>
                     <div className={styles['price-product']}>
-                        <span className={styles.price}>$ 27.89</span>
-                        <span className={styles['price-before']}>$ 30.99</span>
+                        <span className={styles.price}>$ {price}</span>
+                        <span className={styles['price-before']}>$ {priceBefore}</span>
                     </div>
                 </div>
                 <ButtonComponent
