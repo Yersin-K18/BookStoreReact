0e448c0b7a0e648d56b5db0aee8e7bc2543e92e2
HoangDSR <nvh.02021995@gmail.com>
2023-04-26 10:36:21 +0700

    Fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ function App() {
         <div className={styles.App}>
             <div className={styles.header}>
                 <div className={styles['header-left']}>
-                    <img src="/public/assets/menu.svg" alt="menu" />
+                    <img src="/assets/menu.svg" alt="menu" />
                     <SearchBoxComponent />
                 </div>
                 <div className={styles['header-right']}>
                     <div className={styles['header-icon']}>
-                        <img src="/public/assets/account.svg" alt="account" />
-                        <img src="/public/assets/favorite.svg" alt="favorite" />
+                        <img src="/assets/account.svg" alt="account" />
+                        <img src="/assets/favorite.svg" alt="favorite" />
                     </div>
-                    <ButtonComponent icon="/public/assets/logo.svg" text={'Basket'} />
+                    <ButtonComponent icon="/assets/logo.svg" text={'Basket'} />
                 </div>
             </div>
         </div>
