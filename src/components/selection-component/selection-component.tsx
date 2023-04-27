36f9bde91f9891903c1269058ebaaf03208d0e1d
HoangDSR <nvh.02021995@gmail.com>
2023-04-27 22:46:09 +0700

    Done customs selection

--- src/components/selection-component/selection-component.tsx
+++ src/components/selection-component/selection-component.tsx
@@ -1,5 +1,6 @@
 import styles from './selection-component.module.scss';
 import classNames from 'classnames';
+import { useState } from 'react';
 
 export interface SelectionComponentProps {
     className?: string;
@@ -10,16 +11,36 @@ export interface SelectionComponentProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-selection-components-and-templates
  */
 export const SelectionComponent = ({ className }: SelectionComponentProps) => {
+    const [selectedItem, setSelectedItem] = useState<HTMLDivElement | null>(null);
+    const [selectedText, setSelectedText] = useState<string>('Choose a genre');
+
+    function handleListItemClick(event: React.MouseEvent<HTMLDivElement>) {
+        if (selectedItem) {
+            selectedItem.classList.remove(styles['dropdown-list__item_selected']);
+        }
+
+        event.currentTarget.classList.add(styles['dropdown-list__item_selected']);
+
+        setSelectedText(event.currentTarget.textContent || '');
+        setSelectedItem(event.currentTarget);
+    }
+
     return (
         <div className={classNames(styles.dropdown, className)}>
             <div className={styles['dropdown-select']}>
-                <span>Choose a genre</span>
+                <span> {selectedText} </span>
                 <i className="fa fa-caret-down icon"></i>
             </div>
             <div className={styles['dropdown-list']}>
-                <div className={styles['dropdown-list__item']}>Pepsi</div>
-                <div className={styles['dropdown-list__item']}>Coca</div>
-                <div className={styles['dropdown-list__item']}>Monster</div>
+                <div className={styles['dropdown-list__item']} onClick={handleListItemClick}>
+                    Pepsi
+                </div>
+                <div className={styles['dropdown-list__item']} onClick={handleListItemClick}>
+                    Coca
+                </div>
+                <div className={styles['dropdown-list__item']} onClick={handleListItemClick}>
+                    Monster
+                </div>
             </div>
         </div>
     );
